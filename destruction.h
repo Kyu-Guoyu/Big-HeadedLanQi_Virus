c822719ab73787394d3078215ffd4f1403d7b46e
王国宇 <124948687+Kyu-Guoyu@users.noreply.github.com>
2023-12-05 22:34:09 +0800

    Delete destruction.h

--- destruction.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <Windows.h>
-#include <AclAPI.h>
-
-extern void DeleteDirectory(LPWSTR Directory);
-extern void OverWriteDisk();
-extern void SetCriticalProcess();
-extern void CrashOS();
